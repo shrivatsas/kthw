173ec1f34a37d76d542014352ae5084cea5604c2
Shrivatsa <shrivatsa.dev@gmail.com>
2025-07-11 22:49:36 +0530

    full GCP project

---
+++ 02-provision-compute.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Provision a VPC
+# Create a `kubernetes` subnet
+# Create a `kubernetes` firewall rules - internal and external
+# Create a static public IP address for the external API access
+
+# Create 3 compute instances within the subnet for Control plane
+# Create worker instances within the Kubernetes Cluster CIDR range for Worker nodes
+
+# Configure SSH access to the instances
